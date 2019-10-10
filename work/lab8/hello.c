ddb76e8496b5e70a6b93c7043aab24342bc22542
ycdjun@gmail.com <yjun1@cs.binghamton.edu>
2019-10-10 15:04:25 -0400

    allow greeting multiple names

--- work/lab8/hello.c
+++ work/lab8/hello.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int
 main(int argc, const char *argv[])
 {
-  if (argc != 2) {
-    fprintf(stderr,"usage: %s NAME\n", argv[0]);
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s NAME...\n", argv[0]);
     exit(1);
   }
-  printf("hello %s\n", argv[1]);
+  for (int i = 1; i < argc; i++) {
+    printf("hello %s\n", argv[i]);
+  }
 }
