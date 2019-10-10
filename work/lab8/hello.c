d6ff7b881ffb210ba75485e1b53cc265aeb4af51
ycdjun@gmail.com <yjun1@cs.binghamton.edu>
2019-10-10 15:03:17 -0400

    send usage message to stderr

--- work/lab8/hello.c
+++ work/lab8/hello.c
@@ -4,7 +4,7 @@ int
 main(int argc, const char *argv[])
 {
   if (argc != 2) {
-    printf("usage: %s NAME\n", argv[0]);
+    fprintf(stderr,"usage: %s NAME\n", argv[0]);
     exit(1);
   }
   printf("hello %s\n", argv[1]);
